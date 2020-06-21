8fe322146c82612303a78da9b77a55b1caac2f72
MorcilloA <alexy.morcillo@gmail.com>
2020-06-21 16:48:42 +0100

    tri liste items + début plaçage items non-datés

--- components/Calendar_bis.js
+++ components/Calendar_bis.js
@@ -1,8 +1,19 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import WeeklyCalendar from 'react-native-weekly-calendar';
+import moment from "moment";
+// Représente l'heure de réveil quotidienne choisie par l'utilisateur
+const reveil = moment().hour(6).minute(30).utc(1);
+// Représente l'heure de coucher qutidienne chosie par l'utilisateur
+const coucher = moment().hours(23).minutes(30).utc(1);
+
 
 export default function Agenda() {
+    /* 
+    * Représente le tableau qui sera dans le local storage
+    * Il faut qu'il y ait "start", "duration" et "note"
+    * Peu import s'il y a d'autres propriétés sur l'objet ça ne pose pas de problèmes
+    */
     const sampleEvents = [
         { 'start': '2020-06-23 09:00:00', 'duration': '00:20:00', 'note': 'Walk my dog' },
         { 'start': '2020-06-23 10:00:00', 'duration': '01:30:00', 'note': 'Faire les courses' },
@@ -11,24 +22,112 @@ export default function Agenda() {
         { 'start': '2020-06-25 14:00:00', 'duration': '02:00:00', 'note': 'Meeting with client' },
         { 'start': '2020-06-25 19:00:00', 'duration': '01:00:00', 'note': 'Dinner with family' },
         { 'start': '2020-06-26 09:30:00', 'duration': '01:00:00', 'note': 'Schedule 1' },
-        { 'start': '2020-06-26 11:00:00', 'duration': '02:00:00', 'note': 'Schedule 2' },
         { 'start': '2020-06-26 15:00:00', 'duration': '01:30:00', 'note': 'Schedule 3' },
+        { 'start': '2020-06-26 11:00:00', 'duration': '02:00:00', 'note': 'Schedule 2' },
+        { 'start': '2020-06-26 22:00:00', 'duration': '01:00:00', 'note': 'Schedule 5' },
         { 'start': '2020-06-26 18:00:00', 'duration': '02:00:00', 'note': 'Schedule 4' },
-        { 'start': '2020-06-26 22:00:00', 'duration': '01:00:00', 'note': 'Schedule 5' }
+        // Test pour les évènements qui n'ont pas de date mais seulement une durée
+        { 'date': '', 'importance': '2', 'duration': '01:00:00', 'echelle': 'mois', 'note': 'Schedule 6' }
+        // { 'date': '', 'importance': '1', 'duration': '00:30:00', 'echelle': 'journée', 'note': 'Schedule 7' },
+        // { 'date': '', 'importance': '0', 'duration': '02:00:00', 'echelle': 'semaine', 'note': 'Schedule 8' },
+        // { 'date': '', 'importance': '0', 'duration': '02:00:00', 'echelle': 'mois', 'note': 'Schedule 9' },
+        // { 'date': '', 'importance': '1', 'duration': '02:00:00', 'echelle': 'semaine', 'note': 'Schedule 10' },
+        // { 'date': '', 'importance': '1', 'duration': '02:00:00', 'echelle': 'semaine', 'note': 'Schedule 11' }
     ]
 
+    // On tri le tableau selon l'importance ( plus la valeur est élevée plus c'est important )
+    sampleEvents.sort((a, b) => (a.importance < b.importance) ? 1 : -1);
+    // On parcourt l'ensemble du tableau
+    for (let i = 0; i < sampleEvents.length; i++) {
+        // On regarde si la propriété "start" de l'objet est définie
+        if (!sampleEvents[i].hasOwnProperty('start')) {
+            // console.log(sampleEvents[i].note);
+            sampleEvents[i].start = getStartFromDuration(sampleEvents[i].duration, sampleEvents[i].echelle);
+        }
+
+    }
+
+
+    // Un petit tri sur le tableau avant de l'utiliser pour être sûr que tout est classé dans l'ordre de la date
+    sampleEvents.sort((a, b) => (a.start > b.start) ? 1 : -1);
+    /**
+     * Ne rien rajouter entre le tri et le return
+     */
     return (
         <View style={styles.container}>
             <WeeklyCalendar
                 events={sampleEvents}
-                style={{ 
-                    marginTop: 100,
-                    height: "100%" 
+                style={{
+                    marginTop: 80,
+                    height: "90%",
+                    color: "red"
                 }}
                 startWeekday={1}
             />
         </View>
     );
+}
+
+function getStartFromDuration(duration, echelle){
+  
+    var now = moment();
+    var periode;
+
+    // Dans quelle limite de temps l'utilisateur souhaite que sa tâche soit faite
+    switch (echelle) {
+        // Si la tâche doit être faite dans la journée
+        case "journée":
+            // On stocke le moment de la fin de la journée
+            var endOfToday = moment().endOf('day').utc(1);
+            // On enlève à l'heure de la fin de journée l'heure qu'il est pour avoir la différence entre les deux
+            var remainingTime = endOfToday.subtract(now.hours(), 'hours').subtract(now.minutes(), 'minutes');
+            // On stocke l'heure et les minutes obtenues
+            var hour = remainingTime.hour();
+            var minute = remainingTime.minute();
+            // On additionne pour avoir le total en minutes
+            periode = hour*60+minute;
+            break;
+        // Si la tâche doit être faite dans la semaine
+        case "semaine":
+            // On stocke le moment de la fin de la semaine
+            var endOfWeek = moment().endOf('week').utc(1);
+            // On enlève à la fin de la semaine l'heure qu'il est pour avoir la différence entre les deux
+            var remainingTime = endOfWeek.subtract(now.hour(), 'hours').subtract(now.minutes(), 'minutes');
+            // On stocke la différence entre 7(une semaine complète) et le jour actuel de la semaine
+            var day = 7 - now.isoWeekday();
+            // On stocke l'heure et les minutes obtenues
+            var hour = remainingTime.hour();
+            var minute = remainingTime.minute();
+            // On convertit et additionne le tout
+            periode = (day*24*60)+(hour*60)+minute;
+
+            break;
+        // Si la tâche doit être faite dans le mois
+        case "mois":
+            // On stocke le moment de la fin du mois
+            var endOfMonth = moment().endOf('month').utc(1);
+            // On enlève à la fin de la semaine l'heure qu'il est pour avoir la différence entre les deux
+            var remainingTime = endOfMonth.subtract(now.hour(), 'hours').subtract(now.minutes(), 'minutes');
+            // On stocke la différence entre le dernier jour du mois et aujourd'hui
+            var day = endOfMonth.date() - now.date();
+            // On stocke l'heure et les minutes obtenues
+            var hour = remainingTime.hour();
+            var minute = remainingTime.minute();
+            // On convertit et additionne le tout
+            periode = (day*24*60)+(hour*60)+minute;
+            break;
+    
+        default:
+            break;
+    }
+    for (let i = 0; i <= periode; i++) {
+        var exploredMinute = moment(now).add(i, 'minutes');
+        if(exploredMinute.isBefore(reveil) || exploredMinute.isAfter(coucher)){
+            console.log(exploredMinute.format("YYYY-MM-DD HH:mm")+" = c'est pendant le sommeil");
+        }
+        
+    }
+    return("2020-06-20 10:00:00");
 }
 
 const styles = StyleSheet.create({
